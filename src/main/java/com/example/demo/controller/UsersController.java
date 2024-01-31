49e8043ddaf51d394a9e574f390e1ed313aa57a9
HP <HP@LAPTOP-L3CN59NJ>
2024-01-31 15:50:58 +0530

    commit

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.entities.Song;
 import com.example.demo.entities.Users;
+import com.example.demo.services.SongService;
 import com.example.demo.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
@@ -17,6 +22,8 @@ public class UsersController
 {
 	@Autowired
 	UsersService services;
+	@Autowired
+	SongService songService;
 	@PostMapping("/register")
 	public String addUsers(@ModelAttribute Users user)
 	{
@@ -34,18 +41,25 @@ public class UsersController
 	}
 	
 	@PostMapping("/validate")
-	public String validate(@RequestParam("email")String email,@RequestParam("password")String password,HttpSession session)
+	public String validate(@RequestParam("email")String email,
+			                @RequestParam("password")String password,
+			                HttpSession session,Model model)
 	{
 		if(services.validateUser(email, password)==true)
 		{
 			String role=services.getRole(email);
-			session.setAttribute(email, email);
+			session.setAttribute("email", email);
 			if(role.equals("admin"))
 			{
 				return "adminHome";
 			}
 		    else
 		    {
+			    Users user=services.getUser(email);
+			    boolean userStatus=user.isPremium();
+			    List<Song> songsList=songService.fetchAllSongs();
+			    model.addAttribute("songs",songsList);
+			    model.addAttribute("isPremium",userStatus);
 			    return "customerHome";
 		    }
 		}
@@ -54,20 +68,7 @@ public class UsersController
 			return "login";
 		}
 	}
-	/*
-	@GetMapping("/pay")
-	public String pay(@RequestParam String email)
-	{
-		boolean paymentStatus=false;
-		if(paymentStatus==true)
-		{
-			Users user=services.getUser(email);
-			user.setPremium(true);
-			services.updateUser(user);
-		}
-		return "login";
-	}
-	*/
+	
 	
 	@GetMapping("/logout")
 	public String logout(HttpSession session)
